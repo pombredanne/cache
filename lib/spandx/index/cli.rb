9ab01fbf65bd315a7fce9de9aba4c4d798905246
mo khan <mo.khan@gmail.com>
2020-02-02 13:05:08 -0700

    Initial commit

---
+++ lib/spandx/index/cli.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'thor'
+
+module Spandx
+  module Index
+    # Handle the application command line parsing
+    # and the dispatch to various command objects
+    #
+    # @api public
+    class CLI < Thor
+      # Error raised by this runner
+      Error = Class.new(StandardError)
+
+      desc 'version', 'spandx-index version'
+      def version
+        require_relative 'version'
+        puts "v#{Spandx::Index::VERSION}"
+      end
+      map %w(--version -v) => :version
+    end
+  end
+end
---
+++ spec/spandx/index_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Spandx::Index do
+  it "has a version number" do
+    expect(Spandx::Index::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
