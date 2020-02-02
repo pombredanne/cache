6482dd403acfe1d43bc40481aa5e0522005cc5a5
mo khan <mo.khan@gmail.com>
2020-02-02 13:05:08 -0700

    Initial commit

---
+++ lib/spandx/index.rb
@@ -0,0 +1,8 @@
+require "spandx/index/version"
+
+module Spandx
+  module Index
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/spandx/index/version.rb
@@ -0,0 +1,5 @@
+module Spandx
+  module Index
+    VERSION = "0.1.0"
+  end
+end
