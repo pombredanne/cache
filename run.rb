d5d9b5b4a700906ab7b9d01860e98a769d76bc1a
mo khan <mo.khan@gmail.com>
2020-05-01 10:33:40 -0600

    Download full nuget catalogue

--- run.rb
+++ run.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
+=begin
+
+Total pages: 10_094
+Total items: 5_509_765
+[5509765, "https://api.nuget.org/v3/catalog0/page10094.json"]
+
+Template:
+====================================
+| count     | time (s) | guess (s) |
+====================================
+| 1_000     |          |
+| 10_000    |          |
+| 100_000   |          |
+| 1_000_000 |          |
+====================================
+
+No Threads
+====================================
+| count     | time (s) | guess (s) |
+====================================
+| 1_000     | 1.527    | 8413
+| 10_000    | 1.826    | 1006
+| 100_000   | 2.952    | 162
+| 1_000_000 | 19.314   | 106
+====================================
+| 5_509_765 | 91       | N/A       |
+====================================
+=end
+
+
 puts "Installing gems..."
 require 'bundler/inline'
 
@@ -22,9 +52,9 @@ class Command
   end
 
   def run(url)
-    request = Typhoeus::Request.new(url, followlocation: true)
+    request = Typhoeus::Request.new(url, followlocation: true, accept_encoding: 'gzip')
     request.on_complete do |response|
-      yield response, hydra
+      yield response
     end
     hydra.queue(request)
     start!
@@ -35,8 +65,8 @@ class Command
   def start!
     return if @started
 
-    hydra.run
     @started = true
+    hydra.run
   end
 end
 
@@ -45,13 +75,14 @@ class Nuget
 
   def initialize
     @command = Command.new
+    @queue = Queue.new
   end
 
   def run
     command.run('https://api.nuget.org/v3/catalog0/index.json') do |response|
       json = Oj.load(response.body)
-      json['items'].each do |item|
-        fetch_page(item['@id'])
+      0.upto(json['count']) do |n|
+        fetch_page("https://api.nuget.org/v3/catalog0/page#{n}.json")
       end
     end
   end
@@ -60,10 +91,13 @@ class Nuget
     command.run(url) do |response|
       json = Oj.load(response.body)
       json['items'].each do |item|
-        puts item.inspect
+        @queue.enq(item)
       end
+    rescue => error
+      puts error.inspect
     end
   end
 end
 
+GC.disable
 Nuget.new.run
