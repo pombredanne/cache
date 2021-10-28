e10bd186cfd3b182002034719ec6c93342fa5ceb
mo khan <mo@mokhan.ca>
2021-10-27 21:23:27 -0600

    validate eco system parameter

--- main.go
+++ main.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spandx/cache/cache"
 	"github.com/spandx/cache/nuget"
 )
 
 func main() {
-	c := nuget.NewCatalog()
-	cache := cache.NewCache(".index")
-	c.Each(func(item nuget.Dependency) {
-		cache.Write(item.Name, item.Version, []string{item.LicenseExpression})
-	})
-	cache.Tidy()
+	if len(os.Args) == 2 {
+		switch os.Args[1] {
+		case "nuget":
+			c := nuget.NewCatalog()
+			cache := cache.NewCache(".index")
+			c.Each(func(item nuget.Dependency) {
+				cache.Write(item.Name, item.Version, []string{item.LicenseExpression})
+			})
+			cache.Tidy()
+			break
+		default:
+			fmt.Println("Unknown eco-system")
+		}
+	} else {
+		fmt.Println("Please specify an eco system")
+	}
 }
