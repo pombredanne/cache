bbbe91656150f31b3b67104dbc8d3854185eede5
mo khan <mo@mokhan.ca>
2022-04-05 10:01:55 -0600

    refactor: extract pkg dir

--- main.go
+++ main.go
@@ -4,21 +4,21 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/spandx/cache/cache"
-	"github.com/spandx/cache/core"
-	"github.com/spandx/cache/npm"
-	"github.com/spandx/cache/nuget"
+	"github.com/spandx/cache/pkg/cache"
+	"github.com/spandx/cache/pkg/core"
+	"github.com/spandx/cache/pkg/npm"
+	"github.com/spandx/cache/pkg/nuget"
 )
 
 func main() {
 	if len(os.Args) == 2 {
-		items := map[string]core.Catalog{
+		catalogs := map[string]core.Catalog{
 			"nuget": nuget.NewCatalog(),
 			"npm":   npm.NewCatalog(),
 		}
 
 		ecosystem := os.Args[1]
-		catalog := items[ecosystem]
+		catalog := catalogs[ecosystem]
 		if catalog == nil {
 			fmt.Println("Unknown ecosystem")
 		} else {
